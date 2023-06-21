4014f175990a4562a19f338b1f18bfd288f732a7
CESCO <cescovalledev@gmail.com>
2023-06-20 22:40:48 -0600

    polishing search page and finishing section

--- src/heroes/pages/SearchPage.jsx
+++ src/heroes/pages/SearchPage.jsx
@@ -16,6 +16,10 @@ export const SearchPage = () => {
 
   const heroes = useMemo(() => getHeroesByName(q), [q]);
 
+  const showSearch = q.length === 0;
+
+  const showError = q.length > 0 && heroes.length === 0;
+
   const { searchText, onInputChange } = useForm({
     searchText: q,
   });
@@ -50,8 +54,17 @@ export const SearchPage = () => {
         <div className="col-7">
           <h4>Results</h4>
           <hr />
-          <div className="alert alert-primary">Search a Hero</div>
-          <div className="alert alert-danger">
+
+          <div
+            className="alert alert-primary animate__animated animate__fadeIn"
+            style={{ display: showSearch ? "" : "none" }}
+          >
+            Search a Hero
+          </div>
+          <div
+            className="alert alert-danger animate__animated animate__fadeIn"
+            style={{ display: showError ? "" : "none" }}
+          >
             Hero <b>{q}</b> not found
           </div>
           {heroes.map((hero) => (
