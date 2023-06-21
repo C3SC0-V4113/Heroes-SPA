c2b93ee1edd024c96d7391104cac4ca084c065b6
CESCO <cescovalledev@gmail.com>
2023-06-20 22:16:03 -0600

    added search Page and hook to manage the form

--- src/heroes/pages/SearchPage.jsx
+++ src/heroes/pages/SearchPage.jsx
@@ -1,3 +1,56 @@
+// import { HeroCard } from "../components/HeroCard";
+
+import { useLocation, useNavigate } from "react-router-dom";
+import { useForm } from "../../hooks/useForm";
+import queryString from "query-string";
+
 export const SearchPage = () => {
-  return <div>SearchPage</div>;
+  const navigate = useNavigate();
+  const location = useLocation();
+  // console.log(location);
+  const { q = "" } = queryString.parse(location.search);
+
+  const { searchText, onInputChange } = useForm({
+    searchText: "",
+  });
+
+  const onSearchSubmit = (event) => {
+    event.preventDefault();
+    if (searchText.trim().length <= 1) return;
+    navigate(`?q=${searchText.toLowerCase()}`);
+  };
+
+  return (
+    <>
+      <h1>Search</h1>
+      <hr />
+      <div className="row">
+        <div className="col-5">
+          <h4>Searching</h4>
+          <hr />
+          <form onSubmit={onSearchSubmit}>
+            <input
+              type="text"
+              placeholder="Search a hero"
+              className="form-control"
+              name="searchText"
+              autoComplete="off"
+              value={searchText}
+              onChange={onInputChange}
+            />
+            <button className="btn btn-outline-primary mt-3">Search</button>
+          </form>
+        </div>
+        <div className="col-7">
+          <h4>Results</h4>
+          <hr />
+          <div className="alert alert-primary">Search a Hero</div>
+          <div className="alert alert-danger">
+            Hero <b>{q}</b> not found
+          </div>
+          {/* <HeroCard /> */}
+        </div>
+      </div>
+    </>
+  );
 };
