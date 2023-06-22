2cc5d698c70bd36aee06d54fbe9a314b8a243d66
CESCO <cescovalledev@gmail.com>
2023-06-21 23:12:35 -0600

    added context to login with user

--- src/auth/context/AuthProvider.jsx
+++ src/auth/context/AuthProvider.jsx
@@ -2,14 +2,32 @@ import PropTypes from "prop-types";
 import { AuthContext } from "./AuthContext";
 import { useReducer } from "react";
 import { authReducer } from "./authReducer";
+import { types } from "../types";
 
 const initialState = {
   logged: false,
 };
 
 export const AuthProvider = ({ children }) => {
-  const [state, dispatch] = useReducer(authReducer, initialState);
-  return <AuthContext.Provider value={{}}>{children}</AuthContext.Provider>;
+  const [authState, dispatch] = useReducer(authReducer, initialState);
+
+  const login = async (name = "") => {
+    const action = {
+      type: types.login,
+      payload: {
+        id: "ABC",
+        name: name,
+      },
+    };
+
+    dispatch(action);
+  };
+
+  return (
+    <AuthContext.Provider value={{ ...authState, login: login }}>
+      {children}
+    </AuthContext.Provider>
+  );
 };
 
 AuthProvider.propTypes = {
