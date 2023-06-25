5a1d79b8c06df9de9ad89fab5121ba06c4e24b03
CESCO <cescovalledev@gmail.com>
2023-06-24 23:05:45 -0600

    --amend

---
+++ src/test/heroes/components/NavBar.test.jsx
@@ -0,0 +1,5 @@
+describe("Pruebas en <NavBar/>", () => {
+  test("el nombre de la persona logueada es mostrado", () => {});
+
+  test("al dar en click logout se llama el navigate con sus dos argumentos y la funcion logout", () => {});
+});
