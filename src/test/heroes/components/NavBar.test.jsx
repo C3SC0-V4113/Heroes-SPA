7cfe77c90b663d7568ef65dc3eb98159990565df
CESCO <cescovalledev@gmail.com>
2023-06-24 23:26:57 -0600

    Homework of routing done

--- src/test/heroes/components/NavBar.test.jsx
+++ src/test/heroes/components/NavBar.test.jsx
@@ -1,5 +1,60 @@
-describe("Pruebas en <NavBar/>", () => {
-  test("el nombre de la persona logueada es mostrado", () => {});
+import { fireEvent, render, screen } from "@testing-library/react";
+import { AuthContext } from "../../../auth";
+import { NavBar } from "../../../heroes";
+import { MemoryRouter } from "react-router-dom";
 
-  test("al dar en click logout se llama el navigate con sus dos argumentos y la funcion logout", () => {});
+const mockedUsedNavigate = jest.fn();
+
+jest.mock("react-router-dom", () => ({
+  ...jest.requireActual("react-router-dom"),
+  useNavigate: () => mockedUsedNavigate,
+}));
+
+describe("Pruebas en <NavBar/>", () => {
+  test("el nombre de la persona logueada es mostrado", () => {
+    const contextValue = {
+      logged: true,
+      user: {
+        name: "Cecy",
+        id: "ABC123",
+      },
+    };
+
+    render(
+      <AuthContext.Provider value={contextValue}>
+        <MemoryRouter initialEntries={["/"]}>
+          <NavBar />
+        </MemoryRouter>
+      </AuthContext.Provider>
+    );
+
+    // screen.debug();
+    expect(screen.getByText(contextValue.user.name)).toBeTruthy();
+  });
+
+  test("al dar en click logout se llama el navigate con sus dos argumentos y la funcion logout", () => {
+    const contextValue = {
+      logout: jest.fn(),
+      logged: true,
+      user: {
+        name: "Cecy",
+        id: "ABC123",
+      },
+    };
+    render(
+      <AuthContext.Provider value={contextValue}>
+        <MemoryRouter initialEntries={["/"]}>
+          <NavBar />
+        </MemoryRouter>
+      </AuthContext.Provider>
+    );
+
+    const logoutButton = screen.getByRole("button", { name: "Logout" });
+
+    fireEvent.click(logoutButton);
+
+    expect(contextValue.logout).toHaveBeenCalled();
+
+    expect(mockedUsedNavigate).toHaveBeenCalled();
+  });
 });
